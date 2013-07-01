04cc8880ca3f70dbfe1e7e0e4732326f86932dae
Shelby <kilik821@yahoo.com>
2013-07-01 19:15:30 -0400

    Converted to Coffeescript

--- app/public/js/services.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-angular.module('myApp.services', ['ngResource'])
-    .factory('Todo', function ($resource) {
-        return $resource('api/Todo/:id', {}, {
-        });
-    });
